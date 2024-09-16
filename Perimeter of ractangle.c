15e229fda71d883369253199561053687b53ff87
Shivam Sharma <iam_shivam26@Shivams-Laptop.local>
2024-09-17 00:58:05 +0530

    first commit

---
+++ Perimeter of ractangle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+/*
+perimeter of ractangle 
+2l+2b
+2(l+b)
+
+*/
+int l,w;
+printf("enter length and breadth");
+scanf("%d%d" , &l,&w);
+
+
+printf(" perimeter of ractangle is %d" , 2 * l + 2 * w );
+
+
+
+
+return 0;
+}
---
+++ cube.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf(" enter a no.");
+    scanf("%d" , &n);
+    printf("cube of n is %d" , n*n*n);
+
+
+
+
+
+
+return 0;
+}
